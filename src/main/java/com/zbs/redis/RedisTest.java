ff605896d65b932fb604af2ae18faf53ae7b0a12
zbs <zhangbaoshuai@cph.citic>
2023-01-17 15:22:41 +0800

    feat: Jedis连接redis 操作

---
+++ src/main/java/com/zbs/redis/RedisTest.java
@@ -0,0 +1,19 @@
+package com.zbs.redis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * description: RedisTest
+ * date: 2023/1/17 14:39
+ * author: zhangbs
+ * version: 1.0
+ */
+public class RedisTest {
+    public static void main(String[] args) {
+        // 连接本地的Redis服务
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        jedis.auth("root");
+        String response = jedis.ping("ping ping,pong pong");
+        System.out.println(response);
+    }
+}
