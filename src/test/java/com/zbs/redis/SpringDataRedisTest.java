f3d6d757bf14fb3f98a9c077e96bb5ffba2e2a43
zbs <zhangbaoshuai@cph.citic>
2023-01-18 10:42:33 +0800

    feat: SpringDataRedis-String操作

--- src/test/java/com/zbs/redis/SpringDataRedisTest.java
+++ src/test/java/com/zbs/redis/SpringDataRedisTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.TimeUnit;
@@ -24,18 +25,24 @@ public class SpringDataRedisTest {
 
     @Test
     public void testString() {
-
-        redisTemplate.opsForValue().set("zhangsan", "25");
-        String value = (String) redisTemplate.opsForValue().get("zhangsan");
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        // 存值
+        valueOperations.set("zhangsan", "25");
+        // 取值
+        String value = (String) valueOperations.get("zhangsan");
         System.out.println(value);
 
         // 设置key的有效期
-        redisTemplate.opsForValue().set("lisi", "55", 10l, TimeUnit.SECONDS);
+        valueOperations.set("lisi", "55", 100l, TimeUnit.SECONDS);
+
+        // set添加一条数据，不会影响其他key的超时时间！
+        //  valueOperations.set("lisi2", "55", 100l, TimeUnit.SECONDS);
+        // 覆盖数值55，如果不设置过期时间，则默认-1，“不失效”，如果设置会覆盖
+        valueOperations.set("lisi", "56", 100l, TimeUnit.SECONDS);
 
         // setIfAbsent：如果没有就新增，如果有就替换
-        Boolean result = redisTemplate.opsForValue().setIfAbsent("city", "beijing");
+        Boolean result = valueOperations.setIfAbsent("city", "beijing");
         System.out.println(result);
-
     }
 
 }
