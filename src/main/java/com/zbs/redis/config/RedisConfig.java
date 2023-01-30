513ee184b199d2fa99909710bc1b53f28bdc2d56
zbs <zhangbaoshuai@cph.citic>
2023-01-30 17:27:52 +0800

    feat: SpringDataRedis key value数据值序列化

---
+++ src/main/java/com/zbs/redis/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.zbs.redis.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * description: RedisConfig Redis配置类
+ * date: 2023/1/18 11:44
+ * author: zhangbs
+ * version: 1.0
+ */
+@Configuration
+public class RedisConfig {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Bean
+    public void init() {
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+    }
+
+}
